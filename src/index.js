2ccaf57a06fe822f00add99bdec9011d313190f4
Archie Baer <archie@abaer.dev>
2020-07-25 03:06:23 +0100

    Simplified index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-/**
- * Custom Verdaccio Authenticate Plugin.
- */
 class AuthCustomPlugin {
   constructor(config, options) {
     return this;
   }
-  /**
-   * Authenticate an user.
-   * @param user user to log
-   * @param password provided password
-   * @param cb callback function
-   */
+
   authenticate(user, password, cb) {
-    // here your code
+    console.log(user);
+    console.log(password);
+    cb(null, []);
   }
 
-  /**
-   * check grants for such user.
-   */
-  allow_access() {
-    // in case of restrict the access
-  }
+  allow_access() {}
 
-  /**
-   * check grants to publish
-   */
-  allow_publish() {
-    // in cass to check if has permission to publish
-  }
+  allow_publish() {}
 }
 
-module.exports = (config, options) => {
-  return new AuthCustomPlugin(config, options);
-};
+module.exports = (config, options) => new AuthCustomPlugin(config, options);
