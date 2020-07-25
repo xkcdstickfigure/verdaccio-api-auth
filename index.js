9f9a0b9494d3c9a813116909d016de9bb32d78a9
Archie Baer <archie@abaer.dev>
2020-07-25 21:54:55 +0100

    Makes http request for authenticate method

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+const axios = require("axios");
+
 class AuthCustomPlugin {
-    constructor(config, options) {
-      return this;
-    }
-  
-    authenticate(user, password, cb) {
-      console.log(user);
-      console.log(password);
-      cb(null, []);
-    }
-  
-    allow_access() {}
-  
-    allow_publish() {}
-  }
-  
-  module.exports = (config, options) => new AuthCustomPlugin(config, options);
+	constructor(config) {
+		this.url = config.url;
+		return this;
+	}
+
+	authenticate(username, password, cb) {
+		axios
+			.post(`${this.url}/authenticate`, {username, password})
+			.then(res => cb(null, res.data))
+			.catch(() => cb(null, false));
+	}
+}
+
+module.exports = config => new AuthCustomPlugin(config);
