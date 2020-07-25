73b0e9c18a8d9e394be2304cdb893c64c1241ce9
Archie Baer <archie@abaer.dev>
2020-07-25 03:19:52 +0100

    Removed files and simplified module

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/index.js');
+class AuthCustomPlugin {
+    constructor(config, options) {
+      return this;
+    }
+  
+    authenticate(user, password, cb) {
+      console.log(user);
+      console.log(password);
+      cb(null, []);
+    }
+  
+    allow_access() {}
+  
+    allow_publish() {}
+  }
+  
+  module.exports = (config, options) => new AuthCustomPlugin(config, options);
