0767b21d530890bcb71f81beebf338be951ed7d5
Archie Baer <archie@abaer.dev>
2020-07-27 02:23:27 +0100

    allow_access, allow_publish, allow_unpublish

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ class AuthCustomPlugin {
 			.then(res => cb(null, res.data))
 			.catch(() => cb(null, false));
 	}
+
+	allow_access(username, package, cb) {
+		axios
+			.get(`${this.url}/allow_access?user=${username}&package=${package}`)
+			.then(res => cb(null, res.data))
+			.catch(() => cb(null, false));
+	}
+
+	allow_publish(username, package, cb) {
+		axios
+			.get(`${this.url}/authenticate?user=${username}&package=${package}`)
+			.then(res => cb(null, res.data))
+			.catch(() => cb(null, false));
+	}
+
+	allow_unpublish(username, package, cb) {
+		axios
+			.get(`${this.url}/authenticate?user=${username}&package=${package}`)
+			.then(res => cb(null, res.data))
+			.catch(() => cb(null, false));
+	}
 }
 
 module.exports = config => new AuthCustomPlugin(config);
