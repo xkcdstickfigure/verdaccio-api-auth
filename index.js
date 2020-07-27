79f23f64157ae22917b81b915c40a558b95434a8
Archie Baer <archie@abaer.dev>
2020-07-27 02:44:11 +0100

    Changed errors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const {getForbidden} = require("@verdaccio/commons-api");
 
 class AuthCustomPlugin {
 	constructor(config) {
@@ -16,22 +17,34 @@ class AuthCustomPlugin {
 	allow_access(user, pkg, cb) {
 		axios
 			.post(`${this.url}/allow_access`, {user, package: pkg})
-			.then(res => cb(null, res.data))
-			.catch(() => cb(null, false));
+			.then(res =>
+				res.data
+					? cb(null, true)
+					: cb(getForbidden("not allowed to access package"))
+			)
+			.catch(() => cb(getForbidden("not allowed to access package")));
 	}
 
 	allow_publish(user, pkg, cb) {
 		axios
 			.post(`${this.url}/allow_publish`, {user, package: pkg})
-			.then(res => cb(null, res.data))
-			.catch(() => cb(null, false));
+			.then(res =>
+				res.data
+					? cb(null, true)
+					: cb(getForbidden("not allowed to publish package"))
+			)
+			.catch(() => cb(getForbidden("not allowed to publish package")));
 	}
 
 	allow_unpublish(user, pkg, cb) {
 		axios
 			.post(`${this.url}/allow_unpublish`, {user, package: pkg})
-			.then(res => cb(null, res.data))
-			.catch(() => cb(null, false));
+			.then(res =>
+				res.data
+					? cb(null, true)
+					: cb(getForbidden("not allowed to unpublish package"))
+			)
+			.catch(() => cb(getForbidden("not allowed to unpublish package")));
 	}
 }
 
